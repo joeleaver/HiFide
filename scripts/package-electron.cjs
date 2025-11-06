0c11ef71439191b14e7a3d9645038e143d1b6dcf
Joe Leaver <joe@joeleaver.com>
2025-11-06 11:55:47 -0700

    more build fixes

--- scripts/package-electron.cjs
+++ scripts/package-electron.cjs
@@ -20,13 +20,29 @@ function tsStamp(date = new Date()) {
   return `${yyyy}${MM}${dd}-${HH}${mm}${ss}`
 }
 
+function buildEnv() {
+  const env = { ...process.env }
+  // Silence electron-builder's internal npm/pnpm rebuild; we handle rebuild ourselves
+  env.ELECTRON_BUILDER_DISABLE_NPM_REBUILD = 'true'
+  // Workaround: some tools read npm_* vars and try to exec pnpm.cjs directly on Windows
+  if (process.platform === 'win32') {
+    if (env.npm_execpath && /pnpm[\\\/]bin[\\\/]pnpm\.cjs$/i.test(env.npm_execpath)) {
+      delete env.npm_execpath
+    }
+    if (env.npm_config_user_agent && /pnpm/i.test(env.npm_config_user_agent)) {
+      env.npm_config_user_agent = 'npm'
+    }
+  }
+  return env
+}
+
 function runNode(cliJs, args) {
-  const r = spawnSync(process.execPath, [cliJs, ...args], { stdio: 'inherit' })
+  const r = spawnSync(process.execPath, [cliJs, ...args], { stdio: 'inherit', env: buildEnv() })
   return r.status ?? 1
 }
 
 function runBin(bin, args, shell) {
-  const r = spawnSync(bin, args, { stdio: 'inherit', shell: !!shell })
+  const r = spawnSync(bin, args, { stdio: 'inherit', shell: !!shell, env: buildEnv() })
   return r.status ?? 1
 }
 
